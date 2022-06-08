9c5bf9d9d51a29f27cdb89c0b9be015d9cf344cf
wkdgus1164 <wkdgus1164@naver.com>
2022-06-09 02:50:46 +0900

    feat: 로그인 기능 구현

--- app/src/main/java/com/inhatc/welko/IntroActivity.java
+++ app/src/main/java/com/inhatc/welko/IntroActivity.java
@@ -1,14 +1,14 @@
 package com.inhatc.welko;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 
-public class IntroActivity extends AppCompatActivity implements View.OnClickListener{
+import androidx.appcompat.app.AppCompatActivity;
+
+public class IntroActivity extends AppCompatActivity implements View.OnClickListener {
 
     // 1초 동안 intro 화면 노출 후, login 화면으로 이동
     // https://anywaydevlog.tistory.com/24 (참고 자료)
@@ -34,11 +34,11 @@ public class IntroActivity extends AppCompatActivity implements View.OnClickList
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
                 finish();
             }
-        },1000*sec);
+        }, 1000 * sec);
     }
 
     @Override
--- app/src/main/java/com/inhatc/welko/LoginActivity.java
+++ app/src/main/java/com/inhatc/welko/LoginActivity.java
@@ -1,34 +1,108 @@
 package com.inhatc.welko;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
-    private EditText email;
-    private EditText pw;
-    private Button loginBtn;
+@SuppressLint({"ResourceAsColor", "SetTextI18n"})
+public class LoginActivity extends Activity implements View.OnClickListener, TextWatcher {
+
+    private EditText etEmail;
+    private EditText etPassword;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        email = findViewById(R.id.loginEmail);
-        pw = findViewById(R.id.loginPw);
-        loginBtn = findViewById(R.id.loginBtn);
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
 
-//        if (email.getText().toString().length() == 0
-//        || pw.getText().toString().length() == 0) {
-//            loginBtn.setTextColor("#000000");
-//            loginBtn.setBackground();
-//
-//        }
+        btnLogin.setOnClickListener(this);
+        etEmail.addTextChangedListener(this);
+        etPassword.addTextChangedListener(this);
+
+        disableLoginButton();
+    }
+
+    private void disableLoginButton() {
+        btnLogin.setEnabled(false);
+        btnLogin.setBackgroundColor(Color.parseColor("#666666"));
+    }
+
+    private void enableLoginButton() {
+        btnLogin.setEnabled(true);
+        btnLogin.setBackgroundColor(Color.parseColor("#2D51C9"));
+    }
+
+    private void requestMemberList() {
+        String email = etEmail.getText().toString();
+        String password = etPassword.getText().toString();
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        String url = "http://welko.ap-northeast-2.elasticbeanstalk.com/member/login?email=" + email + "&password=" + password;
+
+        StringRequest stringRequest = new StringRequest(
+                Request.Method.GET,
+                url,
+
+                response -> new MaterialAlertDialogBuilder(LoginActivity.this)
+                        .setTitle("Login succeed")
+                        .setMessage(response)
+                        .setPositiveButton("Go home", (dialogInterface, i) -> startActivity(
+                                new Intent(LoginActivity.this, HomeActivity.class)
+                        ))
+                        .show(),
+
+                error -> new MaterialAlertDialogBuilder(LoginActivity.this)
+                        .setTitle("Login failed")
+                        .setMessage("Please check your account information.")
+                        .setPositiveButton("Close", (dialogInterface, i) -> {
+                        })
+                        .show()
+        );
+
+        requestQueue.add(stringRequest);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == btnLogin) requestMemberList();
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
+        String email = etEmail.getText().toString();
+        String password = etPassword.getText().toString();
+
+        boolean isInputValid = email.length() > 0 && password.length() > 0;
+
+        if (isInputValid) enableLoginButton();
+        else disableLoginButton();
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+
     }
 }
