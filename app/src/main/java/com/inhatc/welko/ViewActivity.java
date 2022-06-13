dd23e385ee7cd15654cd4e7de47782d8f280a6f5
sanghho <shhan3896@gmail.com>
2022-06-13 13:49:29 +0900

    feat:구글 맵 수정

--- app/src/main/java/com/inhatc/welko/ViewActivity.java
+++ app/src/main/java/com/inhatc/welko/ViewActivity.java
@@ -116,6 +116,7 @@ public class ViewActivity extends AppCompatActivity {
             public void onMapReady(GoogleMap googleMap) {
                 mMap = googleMap;
 
+
                 // Add a marker in Sydney and move the camera
 //                LatLng sydney = new LatLng(-34, 151);
 //                mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
@@ -133,12 +134,13 @@ public class ViewActivity extends AppCompatActivity {
                         .title(name));
                 marker.showInfoWindow();
 
-                googleMap.setOnMapLoadedCallback(new GoogleMap.OnMapLoadedCallback(){
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(nameLatlng));
+                mMap.animateCamera(CameraUpdateFactory.zoomTo(20));
 
+                googleMap.setOnMapLoadedCallback(new GoogleMap.OnMapLoadedCallback(){
                     @Override
                     public void onMapLoaded() {
-                        mMap.moveCamera(CameraUpdateFactory.newLatLng(nameLatlng));
-                        mMap.animateCamera(CameraUpdateFactory.zoomTo(20));
+
                     }
                 });
             }
